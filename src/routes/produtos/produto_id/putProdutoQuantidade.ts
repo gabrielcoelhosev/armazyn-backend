f36be8288f6c811e2a9e3ca5d5f03449bd7e04b9
gabrielcoelhosev <gabrielcoelhosev.dev@gmail.com>
2025-10-28 09:28:42 -0300

    ajustes finaiss

--- src/routes/produtos/produto_id/putProdutoQuantidade.ts
+++ src/routes/produtos/produto_id/putProdutoQuantidade.ts
@@ -15,13 +15,30 @@ export async function putProdutoQuantidade(req: FastifyRequest, reply: FastifyRe
     const { id } = querySchema.parse(req.params);
     const { quantidade } = bodySchema.parse(req.body);
 
+      let alerta = '';
+      let zerandoem = '';
+
+    if(quantidade > 250){
+        alerta = 'Estoque estável'
+        zerandoem = '2026-01-20T00:00:00Z';
+    } else{
+        alerta = 'Ruptura iminente'
+        zerandoem = '2025-11-15T00:00:00Z';
+    }
+
+    const semana1 = quantidade - 20;
+    const semana2 = quantidade - 50;
+    const semana3 = quantidade - 60;
+    const semana4 = quantidade - 80; 
+
+
     const query: QueryConfig = {
         text: /*sql*/`
             UPDATE produtos
-            SET estoqueatual = $1
-            WHERE id = $2
+            SET estoqueatual = $1, alerta =$2, semana1 = $3, semana2 = $4, semana3 = $5, semana4 = $6, estoquezerandoem = $7
+            WHERE id = $8
         `,
-        values: [quantidade, id]
+        values: [quantidade, alerta, semana1, semana2, semana3, semana4, zerandoem, id]
     }
 
     const updateQuantidade = await postgresHelper.sql(query);
